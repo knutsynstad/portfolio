bc1ce7ba950fe8f127a7426941fddc698d3a8deb
Knut Synstad <knutsynstad@users.noreply.github.com>
2023-09-16 13:39:55 -0700

    Added project url redirects for backwards compatability

--- app/projects/a_pictogram_is_worth_a_thousand_words/page.tsx
+++ app/projects/a_pictogram_is_worth_a_thousand_words/page.tsx
@@ -24,74 +24,73 @@ import Icons_Electronics from "../../../public/images/Icons_Electronics.svg";
 import Icons_People from "../../../public/images/Icons_People.svg";
 
 const data = {
-    id: "a_pictogram_is_worth_a_thousand_words",
-    metadata: [
+  id: "a_pictogram_is_worth_a_thousand_words",
+  metadata: [
+    {
+      title: "Categories",
+      values: [
         {
-            title: "Categories",
-            values: [
-                {
-                    name: "Iconography",
-                },
-            ],
+          name: "Iconography",
         },
-    ],
-    title: "A pictogram is worth a thousand words.",
-    description: (
-        <>
-            <p>
-                Most of us are familiar with the notion that a complex idea may
-                be communicated through an image, or that an image of an object
-                captures its meaning or essence better than a description could.
-                Iconography is a natural extension of this notion. Space
-                efficient and inherently multilingual, it lends itself to a
-                range of communication tasks—from wayfinding on public transit
-                to digital interfaces.
-            </p>
+      ],
+    },
+  ],
+  title: "A pictogram is worth a thousand words.",
+  description: (
+    <>
+      <p>
+        Most of us are familiar with the notion that a complex idea may be
+        communicated through an image, or that an image of an object captures
+        its meaning or essence better than a description could. Iconography is a
+        natural extension of this notion. Space efficient and inherently
+        multilingual, it lends itself to a range of communication tasks—from
+        wayfinding on public transit to digital interfaces.
+      </p>
 
-            <p>
-                The following line icons have a uniform size and were drawn with
-                a single stroke weight, round caps, and corners, on a padded 8
-                by 8 grid. Use of color is limited to a solid color and a
-                derivative tint as needed.
-            </p>
-        </>
-    ),
+      <p>
+        The following line icons have a uniform size and were drawn with a
+        single stroke weight, round caps, and corners, on a padded 8 by 8 grid.
+        Use of color is limited to a solid color and a derivative tint as
+        needed.
+      </p>
+    </>
+  ),
 };
 
 const slides = [
-    [Icons_Core, "Core"],
-    [Icons_Buildings, "Buildings"],
-    [Icons_Transport_Travel, "Transportation & travel"],
-    [Icons_Closed_Loop_Insulin_Delivery2],
-    [Icons_Tech_Science, "Technology & science"],
-    [Icons_Data_Science, "A.I., data & data science"],
-    [Icons_Healthcare, "Healthcare"],
-    [Icons_Article_Publishing2],
-    [Icons_Food_Drink, "Food & drink"],
-    [Icons_Audio_Video, "Audio & video"],
-    [Icons_Household, "Household"],
-    [Icons_Animals_Pets, "Animals & pets"],
-    [Icons_Reseller_Venue_Relationship2],
-    [Icons_People, "People"],
-    [Icons_Office_Supplies, "Office supplies"],
-    [Icons_Time_Date, "Time & date"],
-    [Icons_Feedback_Loop2],
-    [Icons_Finance, "Finance"],
-    [Icons_Communication, "Communication"],
-    [Icons_Electronics, "Electronics"],
+  [Icons_Core, "Core"],
+  [Icons_Buildings, "Buildings"],
+  [Icons_Transport_Travel, "Transportation & travel"],
+  [Icons_Closed_Loop_Insulin_Delivery2],
+  [Icons_Tech_Science, "Technology & science"],
+  [Icons_Data_Science, "A.I., data & data science"],
+  [Icons_Healthcare, "Healthcare"],
+  [Icons_Article_Publishing2],
+  [Icons_Food_Drink, "Food & drink"],
+  [Icons_Audio_Video, "Audio & video"],
+  [Icons_Household, "Household"],
+  [Icons_Animals_Pets, "Animals & pets"],
+  [Icons_Reseller_Venue_Relationship2],
+  [Icons_People, "People"],
+  [Icons_Office_Supplies, "Office supplies"],
+  [Icons_Time_Date, "Time & date"],
+  [Icons_Feedback_Loop2],
+  [Icons_Finance, "Finance"],
+  [Icons_Communication, "Communication"],
+  [Icons_Electronics, "Electronics"],
 ];
 
 const Project = () => (
-    <ProjectDetailsPage data={data}>
-        <div className="flex flex-col gap-32">
-            {slides.map(([src, alt], index) => (
-                <div key={`${alt}+${index}`}>
-                    {alt && <h2 className="mb-8">{alt}</h2>}
-                    <Image src={src} alt={alt} rounded shadow={alt ? true : false} />
-                </div>
-            ))}
+  <ProjectDetailsPage data={data}>
+    <div className="flex flex-col gap-32">
+      {slides.map(([src, alt], index) => (
+        <div key={`${alt}+${index}`}>
+          {alt && <h2 className="mb-8">{alt}</h2>}
+          <Image src={src} alt={alt} rounded shadow={alt ? true : false} />
         </div>
-    </ProjectDetailsPage>
+      ))}
+    </div>
+  </ProjectDetailsPage>
 );
 
 export default Project;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,125 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/a_pictogram_is_worth_a_thousand_words",
+        destination: "/projects/a_pictogram_is_worth_a_thousand_words",
+        permanent: true,
+      },
+      {
+        source: "/automations",
+        destination: "/projects/automations",
+        permanent: true,
+      },
+      {
+        source: "/avatar_builder",
+        destination: "/projects/avatar_builder",
+        permanent: true,
+      },
+      {
+        source: "/faux_code_generator",
+        destination: "/projects/faux_code_generator",
+        permanent: true,
+      },
+      {
+        source: "/gardeners_always_get_their_hands_in_the_dirt",
+        destination: "/projects/gardeners_always_get_their_hands_in_the_dirt",
+        permanent: true,
+      },
+      {
+        source: "/looking_beyond_the_walls",
+        destination: "/projects/looking_beyond_the_walls",
+        permanent: true,
+      },
+      {
+        source: "/mapping_family_caregiving",
+        destination: "/projects/mapping_family_caregiving",
+        permanent: true,
+      },
+      {
+        source: "/measuring_corn_production_in_every_county_every_day",
+        destination:
+          "/projects/measuring_corn_production_in_every_county_every_day",
+        permanent: true,
+      },
+      {
+        source: "/modeling_diabetes",
+        destination: "/projects/modeling_diabetes",
+        permanent: true,
+      },
+      {
+        source: "/moving_mountains_to_shape_the_future",
+        destination: "/projects/moving_mountains_to_shape_the_future",
+        permanent: true,
+      },
+      {
+        source: "/mr_potato_head_is_for_the_birds",
+        destination: "/projects/mr_potato_head_is_for_the_birds",
+        permanent: true,
+      },
+      {
+        source: "/raise_your_glass_to_dorian_gray",
+        destination: "/projects/raise_your_glass_to_dorian_gray",
+        permanent: true,
+      },
+      {
+        source: "/reddit_maps",
+        destination: "/projects/reddit_maps",
+        permanent: true,
+      },
+      {
+        source: "/remedies_come_in_all_shapes_and_flavors",
+        destination: "/projects/remedies_come_in_all_shapes_and_flavors",
+        permanent: true,
+      },
+      {
+        source: "/smarter_testing_for_better_health",
+        destination: "/projects/smarter_testing_for_better_health",
+        permanent: true,
+      },
+      {
+        source: "/the_construction_of_apple_park",
+        destination: "/projects/the_construction_of_apple_park",
+        permanent: true,
+      },
+      {
+        source: "/the_tic_tac_toe_solution_space",
+        destination: "/projects/the_tic_tac_toe_solution_space",
+        permanent: true,
+      },
+      {
+        source: "/the_truth_is_not_always_black_and_white",
+        destination: "/projects/the_truth_is_not_always_black_and_white",
+        permanent: true,
+      },
+      {
+        source: "/using_ai_to_accelerate_life_science",
+        destination: "/projects/using_ai_to_accelerate_life_science",
+        permanent: true,
+      },
+      {
+        source: "/visualizing_the_fortune_500",
+        destination: "/projects/visualizing_the_fortune_500",
+        permanent: true,
+      },
+      {
+        source: "/winding_down_gets_a_little_help",
+        destination: "/projects/winding_down_gets_a_little_help",
+        permanent: true,
+      },
+      {
+        source: "/winning_is_the_only_way_to_play_the_game",
+        destination: "/projects/winning_is_the_only_way_to_play_the_game",
+        permanent: true,
+      },
+      {
+        source: "/without_the_past_there_is_no_future",
+        destination: "/projects/without_the_past_there_is_no_future",
+        permanent: true,
+      },
+    ];
+  },
+};
