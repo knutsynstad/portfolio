9cb3478a19cfce14d455366b73c1653cf6dfe5b4
Knut Synstad <knut@synstad.net>
2026-07-25 13:40:36 -0700

    tidy up

--- app/page.tsx
+++ app/page.tsx
@@ -12,56 +12,56 @@ const projects: Project[] = [
     name: "Mosaic",
     image: "images/mosaic-thumbnail.png",
     url: "/projects/mosaic/",
-    tags: ["Reddit", "Game", "Dev Platform"],
+    tags: ["Reddit", "Community", "Game", "Developer Platform"],
   },
   {
     name: "Syllo",
     image: "images/syllo-thumbnail.png",
     url: "/projects/syllo/",
-    tags: ["Reddit", "Game", "Dev Platform"],
+    tags: ["Reddit", "Community", "Game", "Developer Platform"],
   },
   {
     name: "Pixelary",
     image: "images/pixelary-thumbnail.png",
     url: "/projects/pixelary/",
-    tags: ["Reddit", "Game", "Dev Platform"],
+    tags: ["Reddit", "Community", "Game", "Developer Platform"],
   },
   {
-    name: "Play",
+    name: "Devvit Play",
     image: "images/play-thumbnail.png",
     url: "/projects/play/",
-    tags: ["Reddit", "Tool", "Dev Platform"],
+    tags: ["Reddit", "UI", "Tool", "Developer Platform"],
   },
   // {
-  //   name: "Blocks UI Framework",
+  //   name: "Devvit Blocks",
   //   image: "images/devvit-logo.png",
   //   url: "/projects/blocks/",
-  //   tags: ["Reddit", "Dev Platform"],
+  //   tags: ["Reddit","UI Framework", "Developer Platform"],
   // },
   // Menu Actions
   {
-    name: "Automating Reddit",
+    name: "Reddit Automations",
     image: "images/automations_thumbnail.png",
     url: "/projects/automations/",
-    tags: ["Reddit", "Automation", "Dev Platform"],
+    tags: ["Reddit", "Automation", "Tool", "Developer Platform"],
   },
   {
     name: "Avatar Builder",
     image: "images/AvatarBuilder.png",
     url: "/projects/avatar_builder/",
-    tags: ["Reddit", "Moonshot", "User Economy"],
+    tags: ["Reddit", "Identity", "Tool", "User Economy"],
   },
   {
     name: "Reddit Maps",
     image: "images/reddit-maps-thumbnail.png",
     url: "/projects/reddit_maps/",
-    tags: ["Reddit", "Computational Design", "Solution Space"],
+    tags: ["Reddit", "Computational Design", "Tool", "Solution Space"],
   },
   {
     name: "Faux code generator",
     image: "images/fauxcode-thumbnail.png",
     url: "/projects/faux_code_generator/",
-    tags: ["Tool"],
+    tags: ["Procedural Design" ,"Tool"],
   },
   {
     name: "The Tic-Tac-Toe solution space",
@@ -73,7 +73,7 @@ const projects: Project[] = [
     name: "Visualizing the Fortune 500 by similarity",
     image: "images/Fortune_500_Thumbnail.png",
     url: "/projects/visualizing_the_fortune_500/",
-    tags: ["DDO", "Computational Design", "Solution Space", "Machine Learning"],
+    tags: ["DDO", "Computational Design", "Solution Space", "Tool", "Data Visualization", "Machine Learning"],
   },
   {
     name: "Using A.I. to accelerate life science",
@@ -85,31 +85,31 @@ const projects: Project[] = [
     name: "The construction of Apple Park",
     image: "images/Apple_Thumbnail.png",
     url: "/projects/the_construction_of_apple_park/",
-    tags: ["DDO", "Remote Sensing", "Satellites"],
+    tags: ["DDO", "Data Visualization", "Remote Sensing", "Satellites", "Time-lapse"],
   },
   {
     name: "Mapping family caregiving",
     image: "images/Caremaps_0.png",
     url: "/projects/mapping_family_caregiving/",
-    tags: ["DDO", "Information Design"],
+    tags: ["DDO", "Data Visualization", "Procedural Design"],
   },
   {
     name: "From sensors to action",
     image: "images/Sensors_Thumbnail.svg",
     url: "/projects/from_sensors_to_action/",
-    tags: ["DDO", "Information Design", "Remote Sensing", "Machine Learning"],
+    tags: ["DDO", "Model", "Remote Sensing", "Machine Learning"],
   },
   {
     name: "Modeling diabetes as a feedback loop",
     image: "images/Diabetes_Thumbnail.png",
     url: "/projects/modeling_diabetes/",
-    tags: ["DDO", "Information Design", "Cybernetics"],
+    tags: ["DDO", "Model", "Feedback Loop"],
   },
   {
     name: "A pictogram is worth a thousand words",
     image: "images/Iconography_00.gif",
     url: "/projects/a_pictogram_is_worth_a_thousand_words/",
-    tags: ["Iconography"],
+    tags: ["DDO", "Iconography"],
   },
   {
     name: "Mapping social shopping",
@@ -127,67 +127,67 @@ const projects: Project[] = [
     name: "Measuring corn production, in every county, every day",
     image: "images/Corn_Thumbnail.gif",
     url: "/projects/measuring_corn_production_in_every_county_every_day/",
-    tags: ["DDO", "Information Design", "Remote Sensing"],
+    tags: ["DDO", "Data Visualization", "Remote Sensing", "Satellites"],
   },
   {
     name: "Moving mountains to shape the future",
     image: "images/100_People_00.jpg",
     url: "/projects/moving_mountains_to_shape_the_future/",
-    tags: ["Typography", "AAU", "Student"],
+    tags: ["AAU", "Student", "Typography"],
   },
   {
     name: "Mr. Potato Head is for the birds",
     image: "images/Birdhouse_00.jpg",
     url: "/projects/mr_potato_head_is_for_the_birds/",
-    tags: ["Packaging Design", "AAU", "Student"],
+    tags: ["AAU", "Student", "Packaging Design"],
   },
   {
     name: "The truth is not always black & white",
     image: "images/Margin_of_Error_00.jpg",
     url: "/projects/the_truth_is_not_always_black_and_white/",
-    tags: ["Packaging Design", "Branding", "AAU", "Student"],
+    tags: ["AAU", "Student", "Packaging Design", "Branding"],
   },
   {
     name: "Remedies come in all shapes and flavors",
     image: "images/Earthnuts_00.jpg",
     url: "/projects/remedies_come_in_all_shapes_and_flavors/",
-    tags: ["Packaging Design", "Branding", "AAU", "Student"],
+    tags: ["AAU", "Student", "Packaging Design", "Branding"],
   },
   {
     name: "Winding down gets a little help",
     image: "images/Seagrams_00.jpg",
     url: "/projects/winding_down_gets_a_little_help/",
-    tags: ["Branding", "AAU", "Student"],
+    tags: ["AAU", "Student", "Branding"],
   },
   {
     name: "Gardeners always get their hands in the dirt",
     image: "images/French_Paper_00.jpg",
     url: "/projects/gardeners_always_get_their_hands_in_the_dirt/",
-    tags: ["Packaging Design", "AAU", "Student"],
+    tags: ["AAU", "Student", "Packaging Design"],
   },
   {
     name: "Without the past, there is no future",
     image: "images/Befor_00.jpg",
     url: "/projects/without_the_past_there_is_no_future/",
-    tags: ["Ancestry", "AAU", "Student"],
+    tags: ["AAU", "Student", "Ancestry"],
   },
   {
     name: "Raise your glass to Dorian Gray",
     image: "images/Magnolia_00.jpg",
     url: "/projects/raise_your_glass_to_dorian_gray/",
-    tags: ["Packaging Design", "AAU", "Student"],
+    tags: ["AAU", "Student", "Packaging Design"],
   },
   {
     name: "Looking beyond the walls",
     image: "images/sweet_tooth_thumbnail.jpg",
     url: "/projects/looking_beyond_the_walls/",
-    tags: ["Typography", "AAU", "Student"],
+    tags: ["AAU", "Student", "Typography"],
   },
   {
     name: "Winning is the only way to play the game",
     image: "images/Elite_Raiders_00.jpg",
     url: "/projects/winning_is_the_only_way_to_play_the_game/",
-    tags: ["Book Design", "AAU", "Student"],
+    tags: ["AAU", "Student", "Book Design"],
   },
 ];
 
@@ -195,7 +195,7 @@ export default function Home() {
   return (
     <>
       <header className="mb-32 flex flex-col gap-8">
-        <h1>Hello 👋</h1>
+        <h1>Hello.</h1>
         <p className="text-2xl max-w-2xl tracking-tight">
           I&apos;m{" "}
           <a href="https://www.linkedin.com/in/knutsynstad" target="_blank">
